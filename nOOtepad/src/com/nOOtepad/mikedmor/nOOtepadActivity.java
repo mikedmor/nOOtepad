2158bac9605cb00dff00a0c74fc7fe0b0463c4b4
Michael Morris <mikedmor@gmail.com>
2011-09-21 19:37:54 -0400

    Working on fragment UI

--- nOOtepad/src/com/nOOtepad/mikedmor/nOOtepadActivity.java
+++ nOOtepad/src/com/nOOtepad/mikedmor/nOOtepadActivity.java
@@ -2,12 +2,19 @@ package com.nOOtepad.mikedmor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class nOOtepadActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setListAdapter(ArrayAdapter.createFromResource(getApplicationContext(),
+                R.array.tut_titles, R.layout.list_files));
     }
+
+	private void setListAdapter(ArrayAdapter<CharSequence> createFromResource) {
+		// TODO Auto-generated method stub
+		
+	}
 }

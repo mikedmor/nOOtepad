b4dc1daebab8f1a3a3a86f8762584c09b64db7a3
Michael Morris <mikedmor@gmail.com>
2011-09-22 16:51:20 -0400

    Added updated comments to make easier to understand, and code for the theme

--- nOOtepad/src/com/nOOtepad/mikedmor/nOOtepadActivity.java
+++ nOOtepad/src/com/nOOtepad/mikedmor/nOOtepadActivity.java
@@ -3,6 +3,7 @@ package com.nOOtepad.mikedmor;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,12 +15,20 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class nOOtepadActivity extends Activity implements OnItemClickListener {
-    /** Called when the activity is first created. */
+	
+    //Creates a top menu for save buttons and other options
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu, menu);
+        return true;
+    }
+	
+    // Called when the activity is first created.
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+        //Creates a ListView object then populates it with a string
         ListView l = (ListView) findViewById(R.id.number_list);
         ArrayAdapter<String> numbers = new ArrayAdapter<String>(getApplicationContext(),
                         android.R.layout.simple_list_item_1, 
@@ -29,12 +38,6 @@ public class nOOtepadActivity extends Activity implements OnItemClickListener {
         l.setAdapter(numbers);
         l.setOnItemClickListener(this);
     }
-    
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.top_menu, menu);
-        return true;
-    }
 
     public void onGroupItemClick(MenuItem item) {
         // One of the group items (using the onClick attribute) was clicked
@@ -43,9 +46,7 @@ public class nOOtepadActivity extends Activity implements OnItemClickListener {
     }
 
     
-    /**
-     * Add a Fragment to our stack with n Androids in it
-     */
+    //Add a Fragment to our stack with n Androids in it
     private void stackAFragment(int nAndroids) {
         Fragment f = new FragView(nAndroids);
         
@@ -56,10 +57,24 @@ public class nOOtepadActivity extends Activity implements OnItemClickListener {
         ft.commit();
     }
 
-        /**
-         * Called when a number gets clicked
-         */
+        //Called when a number gets clicked
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 stackAFragment(position + 1);
+        }
+    
+        
+        //Adds an action to the app icon
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case android.R.id.home:
+                    //app icon in Action Bar clicked; go home
+                    Intent intent = new Intent(this, nOOtepadActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
         }
 }
